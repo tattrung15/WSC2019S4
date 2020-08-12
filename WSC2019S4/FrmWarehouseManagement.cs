801fc520dd2cda2e8e772f89e4a3c08f16e444b9
Tất Trung <55653291+tattrung15@users.noreply.github.com>
2020-08-12 17:46:11 +0700

    Edit db

--- WSC2019S4/FrmWarehouseManagement.cs
+++ WSC2019S4/FrmWarehouseManagement.cs
@@ -19,7 +19,7 @@ namespace WSC2019S4
         Session4Entities db = null;
         public bool isEdit;
         DataGridViewRow row;
-
+        int orderID;
         private void FrmWarehouseManagement_Load(object sender, EventArgs e)
         {
             db = new Session4Entities();
@@ -31,13 +31,34 @@ namespace WSC2019S4
             cbDestinationWarehouse.DisplayMember = "Name";
             cbDestinationWarehouse.ValueMember = "ID";
 
-            cbPartName.DataSource = db.Parts.ToList();
+            cbPartName.DataSource = db.OrderItems.Where(i => i.Order.SourceWarehouseID != null).Select(m => m.Part).ToList();
             cbPartName.DisplayMember = "Name";
             cbPartName.ValueMember = "ID";
 
             if (isEdit)
             {
-
+                row = this.Tag as DataGridViewRow;
+                orderID = int.Parse(row.Cells["OrderID"].Value.ToString());
+                Order order = db.Orders.SingleOrDefault(m => m.ID == orderID);
+                cbSourceWarehouse.SelectedValue = order.SourceWarehouseID;
+                cbDestinationWarehouse.SelectedValue = order.DestinationWarehouseID;
+                date.Value = order.Date;
+                var list = db.OrderItems.Where(m => m.OrderID == orderID).Select(x => new
+                {
+                    PartName = x.Part.Name,
+                    BatchNumber = x.BatchNumber,
+                    Amount = x.Amount,
+                    Action = "Remove",
+                    PartID = x.PartID
+                }).ToList();
+                dataGridView.Rows.Clear();
+                int index = 0;
+                foreach(var r in list)
+                {
+                    dataGridView.Rows.Add(r.PartName, r.BatchNumber, r.Amount, r.Action, r.PartID);
+                    dataGridView.Rows[index].DefaultCellStyle.BackColor = Color.Beige;
+                    index++;
+                }
             }
         }
 
